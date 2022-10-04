371afd1be5770e5d9b03ff7d8f954e107985e14f
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-10-03 22:20:26 -0400

    added comments to js, added length to final alert

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -3,9 +3,9 @@
 function generatePassword() {
   //prompt, alerts, and confirms asking to user input and storing them in variables
   var lengthSlct = prompt("How many characters would you like your password to contain? \ (Choose between 8-128 characters)");
-
+  //turns lengthSlct prompt response into a number value
   var length = Number(lengthSlct);
-  
+  //limits responses to numerical values between 8 and 128 only
   if (!length) {
     alert("Please choose a value between 8 and 128!");
     return;
@@ -13,7 +13,7 @@ function generatePassword() {
     alert("Please choose a value between 8 and 128!");
     return; 
   };
-
+  //stores response in boolean data of true or false in variables
   var lowerSlct = confirm("Click OK to confirm including lower case characters.");
 
   var upperSlct = confirm("Click OK to confirm including upper case characters.");
@@ -21,13 +21,13 @@ function generatePassword() {
   var numberSlct = confirm("Click OK to confirm including numeric characters.");
 
   var specialSlct = confirm("Click OK to confirm including special characters.");
-
+  //stops user from preceding if no characters are included
   if (lowerSlct === false && upperSlct === false && numberSlct === false && specialSlct === false) {
     alert("You must choose at least one type of character set!");
     return;
   };
-
-  alert("You chose to include:\nLower case: " + lowerSlct + "\nUpper case: " + upperSlct + "\nNumbers: " + numberSlct + "\nSpecial Characters: " + specialSlct);
+  //notifies users on which character types they chose to include
+  alert("You chose:\nLength: " + length + "\nLower case: " + lowerSlct + "\nUpper case: " + upperSlct + "\nNumbers: " + numberSlct + "\nSpecial Characters: " + specialSlct);
 
   // object containing random generator functions
   var randomFunctions = {
@@ -60,23 +60,23 @@ function generatePassword() {
 
   //final random password generator codes
   var generatedPassword = " ";
-
+  //counts how many were confirmed to include
   var typesCount = lowerSlct + upperSlct + numberSlct + specialSlct;
-  //filters out excluded characters from the array
+  //filters out unconfirmed characters from the array
   var typesArray = [{lowerSlct}, {upperSlct}, {numberSlct}, {specialSlct}].filter
   (
     item => Object.values(item)[0]
   );
-   
+  // for loop which runs the random generator functions for each matching variables storing the confirmed characters
   for (var i = 0; i < length; i += typesCount) {
     typesArray.forEach(type => {
       var functionName = Object.keys(type)[0];
-      
       generatedPassword += randomFunctions[functionName]();
     });
   };
+  // limits the generated password length to the actual user input length
   var finalPassword = generatedPassword.slice(0, [length + 1])
-
+  // pushes the final correctly generated password 
   return finalPassword;
 };
 
@@ -89,10 +89,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+  // prints the password from the generatePassword function onto the page
   passwordText.value = password;
     
 };
 
-// Add event listener to generate button
+// Add event listener to generate button, listens for a click and runs writePassword function
 generateBtn.addEventListener("click", writePassword);
